c251e97a36389daf935d6d430bae7b046ba4ca5f
Margeaux Spring <contact@cheapwebmonkey.com>
2015-11-18 01:46:49 -0500

    commented out even MORE experiments

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
-class Location < ActiveRecord::Base
-	require 'csv'
-	has many :business
-	  geocoded_by :address
-	  after_validation :geocode, :if => :address_changed?
-end
+# class Location < ActiveRecord::Base
+# 	require 'csv'
+# 	has many :business
+# 	  geocoded_by :address
+# 	  after_validation :geocode, :if => :address_changed?
+# end
