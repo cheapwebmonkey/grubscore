edcd0636566a3945bfe8aa2b17013f6a21ce3ea1
Margeaux Spring <contact@cheapwebmonkey.com>
2015-11-22 22:13:39 -0500

    added the ability to add an inspection to app

--- app/controllers/lou_inspections_controller.rb
+++ app/controllers/lou_inspections_controller.rb
@@ -16,16 +16,47 @@ before_action :set_lou_inspections, only: [:show]
   end
 
  
+  def new
+    @lou_inspection = LouInspection.new
+  end
 
   # GET /louinspections/1
   # GET /louinspections/1.json
   def show
     
-    @lou_inspections = LouInspection.find(params[:business_id])
-    @lou_violation = @lou_inspection.lou_violation
+    @lou_inspections = LouInspection.find(params[:id])
+    @businesses = @lou_inspection.lou_violation
     @business = @lou_inspection.business
   
   end
 
+  # POST /lou_inspection
+  # POST /lou_inspection.json
+  def create
+    @lou_inspection = LouInspection.new(lou_inspection_params)
+
+    respond_to do |format|
+      if @lou_inspection.save
+        session[:lou_inspection_id] = @lou_inspection.id
+        format.html { redirect_to root_path, success: 'Thanks for adding your inspection!' }
+        format.json { render action: 'show', status: :created, location: @lou_inspection }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @lou_inspection.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 end
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_lou_inspection
+      @lou_inspection = LouInspection.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def lou_inspection_params
+      params.require(:lou_inspection).permit(:business_id, :score, :date, :visit)
+    end
+
